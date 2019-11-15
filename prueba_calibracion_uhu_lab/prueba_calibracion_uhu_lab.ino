e745c5c8d520bcdd60eaddb4b4ddb275b3365a12
Diego.tejada <diego.tejada@diesia.uhu.es>
2019-11-15 15:34:56 +0100

    archivo final definitivo

--- prueba_calibracion_uhu_lab/prueba_calibracion_uhu_lab.ino
+++ prueba_calibracion_uhu_lab/prueba_calibracion_uhu_lab.ino
@@ -28,16 +28,18 @@ const byte minutes = 30;
 const byte hours = 11;
 
 /* Change these values to set the current initial date */
-const byte day = 14;
+const byte day = 15;
 const byte month = 11;
 const byte year = 19;
 //HX711 constructor (dout pin(verde), sck pin(naranja))
-HX711_ADC basc1(1, 0); //HX711 1
+HX711_ADC basc2(1,2); //HX711 1
+HX711_ADC basc1(3,4); //HX711 1
 #define freq_msg 60 //Frequency (in minutes for message)
 boolean debugger = false;
 const int chipSelect = 7;
-const float calValue_1 = -395.63; // calibration value load cell 1
-const String cabecera = "dia,mes,anio,hora,minuto,basc,sens1,sens2";
+const float calValue_1 = -377.89; // calibration value load cell 1
+const float calValue_2 = -398.72;
+const String cabecera = "dia,mes,anio,hora,minuto,basc1,sens1,basc2,sens2";
 
 
 void setup() {
@@ -51,28 +53,42 @@ void setup() {
   Serial1.begin(9600);
   delay(10);
   
-  //////////////////////////////////////////////////////////Basculas
+  //////////////////////////////////////////////////////////Bascula1
   basc1.begin();
   long stabilisingtime = 2000; // tare preciscion can be improved by adding a few seconds of stabilising time
   basc1.start(stabilisingtime);
   byte basc1_rdy = 0;
   if (debugger) {
     if (basc1.getTareTimeoutFlag()) {
-      Serial.println("Tare timeout, check MCU>HX711 no.1 wiring and pin designations");
+      Serial.println("basc1 Tare timeout, check MCU>HX711 no.1 wiring and pin designations");
     }
   }
   basc1.setCalFactor(calValue_1); // user set calibration value (float)
   if (debugger) {
-    Serial.println("Startup + tare is complete");
+    Serial.println("basc1 Startup + tare is complete");
   }
-
+  ////////////////////////////////////////////////////////////Bascula 2
+    basc2.begin();
+  
+  basc2.start(stabilisingtime);
+  byte basc2_rdy = 0;
+  if (debugger) {
+    if (basc2.getTareTimeoutFlag()) {
+      Serial.println("basc2 Tare timeout, check MCU>HX711 no.1 wiring and pin designations");
+    }
+  }
+  basc2.setCalFactor(calValue_2); // user set calibration value (float)
+  if (debugger) {
+    Serial.println("basc2 Startup + tare is complete");
+  }
+//////////////////////////////////////////////////////////////RTC
   rtc.begin(); // initialize RTC
   rtc.setTime(hours, minutes, seconds);
   rtc.setDate(day, month, year);
     if (debugger) {
     Serial.println("RTC started: date and time are set");
   }
-
+//////////////////////////////////////////////////////////////SD
  if (!SD.begin(chipSelect)) {
     Serial.println("Card failed, or not present");
     // don't do anything more:
@@ -89,32 +105,39 @@ void setup() {
 
 
 void loop() {
-  float a, b, c;
+  float a, b, c, d;
   String datos="";
   basc1.begin();
   basc1.setCalFactor(calValue_1);
   a=0;
   b=0;
   c=0;
+  d=0;
   for (int i = 0; i < 100; i++) {
     basc1.update();
     delay(100);
     a += basc1.getData();
     b += analogRead(A0);
-    c += analogRead(A1);
+    
+    basc2.update();
+    delay(100);
+    c += basc2.getData();
+    d += analogRead(A1);
   }
   a /= 100;
   b /= 100;
   c /= 100;
+  d /= 100;
   
-  datos=String(rtc.getDay())+","+String(rtc.getMonth())+","+String(rtc.getYear())+","+String(rtc.getHours())+","+String(rtc.getMinutes())+","+String(a)+","+String(b)+","+String(c);
+  datos=String(rtc.getDay())+","+String(rtc.getMonth())+","+String(rtc.getYear())+","+String(rtc.getHours())+","+String(rtc.getMinutes())+","+String(a)+","+String(b)+","+String(c)+","+String(d);
 
   if (debugger) {
     Serial.println("--------------------------------------------------------------------------------------------------------------");
     Serial.println("medida desde los sensores:");
-    Serial.println("bascula: " + String(a));
+    Serial.println("bascula1: " + String(a));
     Serial.println("sensor A0: " + String(b));
-    Serial.println("sensor A1: " + String(c));
+    Serial.println("bascula2: " + String(c));
+    Serial.println("sensor A1: " + String(d));
     Serial.println("--------------------------------------------------------------------------------------------------------------");
     Serial.println("escribo en la SD:");
     Serial.println(datos);
